36ae1c6a95a6d208a817504af855565d97bc5f4f
Dmytro Danylyk <dmytrodanylyk@gmail.com>
2017-01-21 22:46:38 +0200

    Strict Mode for debug build

---
+++ app/src/main/java/com/dd/template/TemplateApplication.java
@@ -0,0 +1,23 @@
+package com.dd.template;
+
+import android.app.Application;
+import android.os.StrictMode;
+
+public class TemplateApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build());
+        }
+    }
+}
